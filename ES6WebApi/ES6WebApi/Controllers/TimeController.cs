8e8cd7f3fd4117f2bf724cd581097663f764596a
João Leitão <joaoleitao240@gmail.com>
2023-10-11 22:14:45 +0100

    [EG6-9] Homepage

---
+++ ES6WebApi/ES6WebApi/Controllers/TimeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ES6WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet("Server")]
+
+        public async Task<ActionResult> GetServDerTimeAsync()
+        {
+            return Ok(new
+            {
+                ServerTime = DateTime.UtcNow
+            });
+        }
+
+    }
+}
