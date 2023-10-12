5a0e9599f0e9a3266fc219726a2a9791df976fdc
MrFantao <filipe.fa.antao.fa@gmail.com>
2023-10-12 22:32:30 +0100

    Beginning oof tests

---
+++ ES6WebApi/Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests;
+
+using ES6WebApi.Controllers.TimeController
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void GetTime()
+    {
+
+        TimeController
+
+        controller.Request = new HttpRequestMessage();
+        controller.Configuration = new HttpConfiguration();
+
+        // Act
+        var response = controller.Get(10);
+
+        // Assert
+        Assert.IsTrue(response.TryGetContentValue<Product>(out product));
+        Assert.AreEqual(10, product.Id);
+    }
+}
