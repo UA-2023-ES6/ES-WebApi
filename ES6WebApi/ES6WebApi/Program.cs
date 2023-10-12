392bdadf3287968dc460d94aaaf84c66be560585
João Leitão <joaoleitao240@gmail.com>
2023-10-12 22:19:40 +0100

    [EG6-9] Homepage - add DateTimeProvider

--- ES6WebApi/ES6WebApi/Program.cs
+++ ES6WebApi/ES6WebApi/Program.cs
@@ -1,3 +1,6 @@
+using ES6WebApi.Providers;
+using ES6WebApi.Providers.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDateTimeProvider, DateTimeProvider>();
 
 var app = builder.Build();
 
