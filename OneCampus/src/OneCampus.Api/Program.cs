3f9cbad22f85d6f9bdd5492aa6c0b1a999cce27f
MrFantao <filipe.fa.antao.fa@gmail.com>
2023-11-25 21:24:22 +0000

    fixed bug

--- OneCampus/src/OneCampus.Api/Program.cs
+++ OneCampus/src/OneCampus.Api/Program.cs
@@ -23,6 +23,10 @@ builder.Services.AddServices(builder.Configuration);
 builder.Services.AddScoped<IQuestionService, QuestionService>();
 builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
 
+builder.Services.AddScoped<IAnswerService, AnswerService>();
+builder.Services.AddScoped<IAnswerRepository, AnswerRepository>();
+
+
 
 builder.Services.AddCors();
 
